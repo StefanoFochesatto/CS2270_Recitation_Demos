b457b18b2d2f6625c7982c1d680a515309d6fd13
StefanoFochesatto <stefano.gfochesatto@gmail.com>
2025-09-18 14:26:26 +0000

    added a classes demo

---
+++ Classes.cpp
@@ -0,0 +1,49 @@
+#include "Classes.h" // Include the class declaration
+#include <cmath>     // For M_PI, the value of pi
+#include <iostream>
+
+// Its just like classes in python
+// Defining the 'Circle' constructor
+Circle::Circle(double r) {
+    radius = r;
+}
+// member functions are like methods
+// Defining the 'setRadius' member function (or method)
+void Circle::setRadius(double r) {
+    if (r >= 0) {
+        radius = r;
+    }
+}
+
+// Defining the 'calculateArea' member function (or method)
+double Circle::calculateArea() {
+    // This function can directly access the 'radius' member variable
+    // of the object it was called on.
+    return M_PI * radius * radius;
+}
+
+int main(){
+    // --- 1. Constructing a Circle Object ---
+    // We create an instance of the Circle class called 'my_circle'.
+    // The value 5.0 is passed to the constructor.
+    Circle my_circle(5.0);
+    std::cout << "Circle object created with radius 5.0" << std::endl;
+
+    // --- 2. Calling Member Functions (Methods) ---
+    // We use the dot (.) operator to call a function ON the object.
+
+    // Call calculateArea() on my_circle and store the result.
+    double area = my_circle.calculateArea();
+    std::cout << "The initial area is: " << area << std::endl;
+    std::cout << "---" << std::endl;
+
+    // Now, let's modify the object's state.
+    std::cout << "Changing the radius to 10.0..." << std::endl;
+    
+    // Call setRadius() to change the internal 'radius' variable.
+    my_circle.setRadius(10.0);
+
+    // Call calculateArea() again to see the new result.
+    area = my_circle.calculateArea();
+    std::cout << "The new area is: " << area << std::endl;
+}
---
+++ Classes.h
@@ -0,0 +1,15 @@
+class Circle {
+private:
+    // Member variable
+    double radius;
+
+public:
+    // Constructor (a special member function)
+    Circle(double r);
+
+    // A member function (or method) to set the radius
+    void setRadius(double r);
+
+    // A member function (or method) to calculate the area
+    double calculateArea();
+};
